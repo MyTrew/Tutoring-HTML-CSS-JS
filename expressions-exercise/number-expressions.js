0a9285043f933272829e8438139dfdf8c0ba0cdc
MyTrew <59497277+MyTrew@users.noreply.github.com>
2022-04-17 18:04:37 -0400

    tutoring files

---
+++ expressions-exercise/number-expressions.js
@@ -0,0 +1,16 @@
+console.log(2 +3); // 5
+console.log(10 - 15); // -5
+console.log(4 + 1 - 5); //0
+console.log(4 * 3); //12
+console.log(7 / 2); //3.5
+console.log(4 + 2 * 3); //10
+console.log((4 + 2) * 3); //18
+console.log(5 % 2); //r1
+console.log(6 % 2); //r0
+console.log(7 % 2); //r1
+console.log(8 % 2); //r0
+console.log(19 % 8); //r3
+console.log(24 % 8); //r0
+console.log(7 % 4); //r3
+console.log(5 +(10 % 5)); //r5
+console.log((5 + 10) % 5); //r0
---
+++ my-first-program/greeting.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+let myName = "John, "
+let age = 40
+let sex = ", Male,"
+let status = " Worker,"
+let potential = " limitless, "
+let bored = "it happens"
+
+console.log(myName + age + sex + status + potential + bored);
