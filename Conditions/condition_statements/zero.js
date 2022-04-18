8b8ed267edb1ffd0423b00020fd24feb552b146e
John <support@mytrew.com>
2022-04-18 07:07:59 -0400

    condition

---
+++ Conditions/condition_statements/zero.js
@@ -0,0 +1,33 @@
+// if 
+// if  else
+// if else if else
+
+if(true) {
+    console.log("code in if block will execute when true")
+}
+
+
+
+if(false) {
+    console.log("if false code will not execute")
+} else {
+    console.log("code in else is going to execute when if is false")
+}
+
+if( 3 > 6) {
+    console.log("only execute when true if")
+} else if(6 > 7) {
+    console.log("code will execute when its  true else if")
+} else {
+    console.log("if both is false then else  code will execute")
+}
+
+if (true) {
+    console.log("foo");
+  }
+
+  if (false) {
+    console.log("bar");
+  }
+
+  
